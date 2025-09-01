c2367a669baa45cf3aaed8907444f3bdfaa0ce06
Satendra Rajput <satendracaria@gmail.com>
2025-09-01 21:49:21 +0530

    router updated

--- src/app/features/npm-package/npm.routes.server.ts
+++ src/app/features/npm-package/npm.routes.server.ts
@@ -8,8 +8,7 @@ export const NPM_SERVER_ROUTES: ServerRoute[] = [
   {
     path: 'npm-package/:id',
     renderMode: RenderMode.Server // SSR only (no prerender)
-    // Agar prerender karna ho to yahan getPrerenderParams de sakte ho
-    // getPrerenderParams: () => [{ id: '1' }, { id: '2' }]
+  
   },
 
 ];
