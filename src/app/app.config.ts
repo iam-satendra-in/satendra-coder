2bac09909907b1c0510edbae4f70bc86e899fef4
Satendra Rajput <satendracaria@gmail.com>
2025-09-16 12:54:51 +0530

    hii

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, isDevMode } from '@angular/core';
-import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+  isDevMode,
+} from '@angular/core';
+import {
+  provideRouter,
+  withInMemoryScrolling,
+  withViewTransitions,
+} from '@angular/router';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { iloadersInterceptor } from './core/interceptor/loader/iloaders-interceptor';
 import { provideServiceWorker } from '@angular/service-worker';
 
@@ -14,19 +30,22 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideAnimations(),
-    provideHttpClient(withFetch(), withInterceptors([iloadersInterceptor, iauthInterceptor])),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([iloadersInterceptor, iauthInterceptor])
+    ),
     provideClientHydration(withEventReplay()),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerWhenStable:30000',
     }),
-  provideRouter(
-  routes,
-  withViewTransitions(),
-  withInMemoryScrolling({
-    scrollPositionRestoration: 'enabled',
-    anchorScrolling: 'enabled'
-  })
-)
-  ]
+    provideRouter(
+      routes,
+      withViewTransitions(),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      })
+    ),
+  ],
 };
