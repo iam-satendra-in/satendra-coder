22eb64d71bd834f6fbe57dcba2932f1faa990d0a
Satendra Rajput <satendracaria@gmail.com>
2025-09-17 08:52:36 +0530

    updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { CToaster } from './shared/components/global/c-toaster/c-toaster';
 import { CLoading } from './shared/components/global/c-loading/c-loading';
 import { SSeo } from './core/service/other/seo/s-seo';
+import { ScAngularToastify, ToastService } from 'sc-angular-toastify';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,16 @@ import { SSeo } from './core/service/other/seo/s-seo';
 export class App {
   protected title = 'satendra-coder';
 
+  constructor(private toast: ToastService) {}
+
   // This will catch browser/tab close
   @HostListener('window:beforeunload', ['$event'])
   unloadHandler(event: BeforeUnloadEvent) {
     event.preventDefault();
     event.returnValue = 'Are you sure you want to leave?';
+  }
+
+  show() {
+    this.toast.show('Hello World!', 'warn', 2000);
   }
 }
