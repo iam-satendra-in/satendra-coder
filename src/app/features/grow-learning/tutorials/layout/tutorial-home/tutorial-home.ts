0cd0b258f58d055b7617ba95b4a09f4821fef078
Satendra Rajput <satendracaria@gmail.com>
2025-09-25 14:09:15 +0530

    working on tutorial

--- src/app/features/grow-learning/tutorials/layout/tutorial-home/tutorial-home.ts
+++ src/app/features/grow-learning/tutorials/layout/tutorial-home/tutorial-home.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MateriallistModule } from '../../../../../shared/materiallist/materiallist-module';
 
 @Component({
   selector: 'app-tutorial-home',
-  imports: [],
+  imports: [MateriallistModule],
   templateUrl: './tutorial-home.html',
-  styleUrl: './tutorial-home.scss'
+  styleUrl: './tutorial-home.scss',
 })
-export class TutorialHome {
-
-}
+export class TutorialHome {}
