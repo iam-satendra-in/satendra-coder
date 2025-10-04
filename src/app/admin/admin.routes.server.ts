492de900ad85d308fa9e8f7fb13ef3991d752ab4
Satendra Rajput <satendracaria@gmail.com>
2025-10-04 20:37:21 +0530

    server error

--- src/app/admin/admin.routes.server.ts
+++ src/app/admin/admin.routes.server.ts
@@ -1,13 +1,18 @@
 import { ServerRoute, RenderMode } from '@angular/ssr';
 
 export const ADMIN_SERVER_ROUTES: ServerRoute[] = [
-  { path: '', renderMode: RenderMode.Server },
-  { path: 'profile', renderMode: RenderMode.Server },
-  { path: 'users', renderMode: RenderMode.Server },
-  { path: 'settings', renderMode: RenderMode.Server },
+  // // Dashboard & Profile
+  // { path: '', renderMode: RenderMode.Server },
+  // { path: 'profile', renderMode: RenderMode.Server },
+  // { path: 'users', renderMode: RenderMode.Server },
+  // { path: 'settings', renderMode: RenderMode.Server },
+
+  // Tutorials
   { path: 'tutorials', renderMode: RenderMode.Server },
   { path: 'tutorials/create', renderMode: RenderMode.Server },
   { path: 'tutorials/edit/:id', renderMode: RenderMode.Server },
+
+  // Courses
   { path: 'courses', renderMode: RenderMode.Server },
   { path: 'courses/create', renderMode: RenderMode.Server },
   { path: 'courses/edit/:id', renderMode: RenderMode.Server },
