880e8e89955f361c16ac599099a5734551987ae7
Satendra Rajput <satendracaria@gmail.com>
2025-10-04 20:20:23 +0530

    update server ssr

---
+++ src/app/admin/admin.routes.server.ts
@@ -0,0 +1,31 @@
+import { ServerRoute, RenderMode } from '@angular/ssr';
+
+export const ADMIN_SERVER_ROUTES: ServerRoute[] = [
+  // Tutorials Management Routes
+  {
+    path: 'tutorials',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'tutorials/create',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'tutorials/edit/:id',
+    renderMode: RenderMode.Server,
+  },
+
+  // Courses Management Routes
+  {
+    path: 'courses',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'courses/create',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'courses/edit/:id',
+    renderMode: RenderMode.Server,
+  },
+];
--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,11 +2,13 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 import { BLOG_SERVER_ROUTES } from './features/blog/blog.routes.server';
 import { TUTORIAL_SERVER_ROUTES } from './features/grow-learning/tutorials/tutorial.routes.server';
 import { EBOOK_SERVER_ROUTES } from './features/ebook/ebook.routes.server';
+import { ADMIN_SERVER_ROUTES } from './admin/admin.routes.server';
 
 export const serverRoutes: ServerRoute[] = [
   ...BLOG_SERVER_ROUTES,
   ...TUTORIAL_SERVER_ROUTES,
   ...EBOOK_SERVER_ROUTES,
+  ...ADMIN_SERVER_ROUTES,
 
   {
     path: '**',
