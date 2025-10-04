4944236ec132c6119352cdbeddb26fb8add2cad2
Satendra Rajput <satendracaria@gmail.com>
2025-10-04 20:30:19 +0530

    update_2

--- src/app/admin/admin.routes.server.ts
+++ src/app/admin/admin.routes.server.ts
@@ -2,30 +2,12 @@ import { ServerRoute, RenderMode } from '@angular/ssr';
 
 export const ADMIN_SERVER_ROUTES: ServerRoute[] = [
   // Tutorials Management Routes
-  {
-    path: 'tutorials',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'tutorials/create',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: 'tutorials/edit/*',
-    renderMode: RenderMode.Server,
-  },
+  { path: 'tutorials', renderMode: RenderMode.Server },
+  { path: 'tutorials/create', renderMode: RenderMode.Server },
+  { path: 'tutorials/edit/:id', renderMode: RenderMode.Server },
 
   // Courses Management Routes
-  {
-    path: 'courses',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'courses/create',
-    renderMode: RenderMode.Client,
-  },
-  {
-    path: 'courses/edit/*',
-    renderMode: RenderMode.Server,
-  },
+  { path: 'courses', renderMode: RenderMode.Server },
+  { path: 'courses/create', renderMode: RenderMode.Server },
+  { path: 'courses/edit/:id', renderMode: RenderMode.Server },
 ];
